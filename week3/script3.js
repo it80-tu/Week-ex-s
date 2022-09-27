0e81e1f48e56fd2a8dabf8bc80a63aed30ef09c6
Vbif322 <hxdfue@yandex.ru>
2022-09-27 20:52:13 +0300

    week3 stage 4 almost complete

--- week3/script3.js
+++ week3/script3.js
@@ -1,6 +1,8 @@
 let municipallities = {};
 let values = [];
-const table = document.getElementsByTagName('tbody');  
+let mun = [];
+let employment = [];
+const table = document.querySelector('.data');  
     function status(response) {  
         if (response.status >= 200 && response.status < 300) {  
           return Promise.resolve(response)  
@@ -15,20 +17,31 @@ const table = document.getElementsByTagName('tbody');
     fetch('https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff')  
     .then(status)  
     .then(json)  
-    .then(function(data) {  
-      console.log('Request succeeded with JSON response', data);
+    .then(function(data) {
       municipallities = data.dataset.dimension.Alue.category.label;
       data.dataset.value.map(val=>values.push(val));
-      console.log(values);
-      console.log(Object.values(municipallities));            
+      Object.values(municipallities).map(el=>mun.push(el))
             for (let i=0;i<values.length;i++){
                 let row = document.createElement('tr')
-                row.innerHTML= `<td>${Object.values(municipallities)[i]}<td/><td>${values[i]}</td>`
+                row.innerHTML= `<td>${mun[i]}</td><td>${values[i]}</td>`
                 table.appendChild(row);
-                console.log(i);
             }
 
     }).catch(function(error) {  
       console.log('Request failed', error);  
     });
 
+
+    fetch('https://statfin.stat.fi/PxWeb/sq/5e288b40-f8c8-4f1e-b3b0-61b86ce5c065')  
+    .then(status)  
+    .then(json)
+    .then(function(data) {
+      data.dataset.value.map(val=>employment.push(val));
+      for (let i=0;i<employment.length;i++){
+        let newTd = document.createElement('td')
+        newTd.textContent = employment[i];
+        document.getElementsByTagName('tr').appendChild(newTd);
+    }
+    }).catch(function(error) {  
+      console.log('Request failed', error);  
+    });
