5883896d8897c92ba467ccca56b9f208d2f3470a
Vbif322 <hxdfue@yandex.ru>
2022-09-28 18:15:50 +0300

    week4 stage 3 complete

--- week4/script4.js
+++ week4/script4.js
@@ -9,14 +9,15 @@ function status(response) {
 function json(response) {  
   return response.json()  
 }
-function fetching(){
-  fetch('https://api.tvmaze.com/search/shows?q=friends')  
+function fetching(id,word){
+  fetch(`https://api.tvmaze.com/search/shows?q=${word}`)  
 .then(status)  
 .then(json)  
 .then(function(data) {
-    newDiv = createElement('img');
-    
+    console.log(data)
+    document.querySelector('.show-data').innerHTML = `<img src="${data[id].show.image.medium}"><div class="show-info"><h1>${data[id].show.name}</h1><p>${data[id].show.summary}</p></div>`  
 }).catch(function(error) {  
 console.log('Request failed', error);  
 });
 }
+fetching(0,'friends');
