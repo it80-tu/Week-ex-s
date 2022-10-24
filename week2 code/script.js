2c77ff501456895a6da4db046c4d31941f8b5808
Vbif322 <hxdfue@yandex.ru>
2022-10-24 13:16:11 +0300

    week2 code

--- week2 code/script.js
+++ week2 code/script.js
@@ -1,34 +1,68 @@
-if (document.readyState !== "loading") {
-  console.log("Document is ready!");
-  initializeCode();
-} else {
-  document.addEventListener("DOMContentLoaded", function () {
-    console.log("Document is ready after waiting!");
-    initializeCode();
-  });
-}
+let username = ['Webmaster', 'User123', 'AnotherUser222'];
+let email = ['example@email.com','example@email.com','example@email.com'];
+let address = ['Demoland123', 'UserPlace321', 'AnotherPlace21'];
+let admin = [true,false,false];
+let table = document.querySelector('.table');
+const notes = document.getElementById("notes");
+
+const Render = () => {
+    table.innerHTML = '';
+    for (let i=0; i<username.length;i++){
+        table.innerHTML+= `<tr>
+                                <td>${username[i]}</td>
+                                <td>${email[i]}</td>
+                                <td>${address[i]}</td>
+                                <td>${admin[i] ? 'X': '-'}</td>
+                           </tr>`
+    };
+};
+Render();
+
+// if (document.readyState !== "loading") {
+//   console.log("Document is ready!");
+//   Render();
+// } else {
+//   document.addEventListener("DOMContentLoaded", function () {
+//     console.log("Document is ready after waiting!");
+//     Render();
+//   });
+// }
 
 let noteCounter = 0;
+const unameValue = document.getElementById('uname');
+const mailValue = document.getElementById('mail');
+const addressValue = document.getElementById('address');
+const adminValue = document.getElementById('admin');
+const messageValue = document.getElementById('message');
+const addNoteButton = document.getElementById("add-note");
+const removeNoteButton = document.getElementById("remove-note");
 
-function initializeCode() {
-  const addNoteButton = document.getElementById("add-note");
-  const removeNoteButton = document.getElementById("remove-note");
+const AddNote = () => {
+    username.push(unameValue.value);
+    email.push(mailValue.value);
+    address.push(addressValue.value);
+    admin.push(adminValue.checked);
 
-  addNoteButton.addEventListener("click", function () {
     const notes = document.getElementById("notes");
-
     let newParagraph = document.createElement("p");
 
     newParagraph.innerText =
       ++noteCounter + ". " + document.getElementById("message").value;
 
     notes.appendChild(newParagraph);
-  });
 
-  removeNoteButton.addEventListener("click", function () {
-    const notes = document.getElementById("notes");
+    Render();
+};
 
+const RemoveNote = () => {
     notes.removeChild(notes.lastChild);
+    username.pop(unameValue.value);
+    email.pop(mailValue.value);
+    address.pop(addressValue.value);
+    admin.pop(adminValue.checked);
     noteCounter--;
-  });
+    Render();
 }
+
+addNoteButton.addEventListener("click",AddNote);
+removeNoteButton.addEventListener("click",RemoveNote);
